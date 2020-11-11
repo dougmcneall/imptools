1b4d3f65445ea1b600df27d431f9f9101338135a
dougmcneall <dougmcneall@gmail.com>
2020-11-11 16:32:13 +0000

    removing '.' in functions

--- R/impl.R
+++ R/impl.R
@@ -1,23 +1,23 @@
 #' Calculate Implausibility
 #'
 #' @param em Emulator mean
-#' @param em.sd Emulator uncertainty (standard deviation)
+#' @param em_sd Emulator uncertainty (standard deviation)
 #' @param disc Model discrepancy mean
-#' @param disc.sd Model discrepancy uncertainty (standard deviation)
+#' @param disc_sd Model discrepancy uncertainty (standard deviation)
 #' @param obs Observation mean
-#' @param obs.sd Observation uncertainty (standard deviation)
+#' @param obs_sd Observation uncertainty (standard deviation)
 #'
 #' @return Implausibility score
 #' @export
 #'
 #' @examples
-impl <- function(em, em.sd, disc, disc.sd, obs, obs.sd){
+impl <- function(em, em_sd, disc, disc_sd, obs, obs_sd){
   # implausibility function
   # All uncertainties should be expressed as a single standard deviation.
 
-  impl.squared <-  (em - disc - obs)^2 / (em.sd^2 + disc.sd^2 + obs.sd^2)
+  impl_squared <-  (em - disc - obs)^2 / (em_sd^2 + disc_sd^2 + obs_sd^2)
 
-  impl <- sqrt(impl.squared)
+  impl <- sqrt(impl_squared)
 
   impl
 }
